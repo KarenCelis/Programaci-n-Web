275a6b2e29be364abc2fafc6e527b1133247f344
Karen Celis <k-celis@javeriana.edu.co>
2020-02-04 15:20:16 -0500

    JavaScript with functions

---
+++ JavaScript/WorkShop2/index.js
@@ -0,0 +1,82 @@
+function saludar() {
+    var saludo = prompt("Ingrese su nombre");
+    alert("Hola " + saludo);
+}
+
+function operaciones_aritmeticas() {
+    var num1 = parseInt(prompt("Ingrese un numero: "));
+    var num2 = parseInt(prompt("Ingrese un numero"));
+    alert("EL resultado de la suma es " + (num1 + num2));
+    alert("EL resultado de la resta es " + (num1 - num2));
+    alert("EL resultado de la multiplicacion es " + (num1 * num2));
+    alert("EL resultado del modulo es " + (num1 % num2));
+}
+
+function area_cuadrado() {
+
+    var base = parseInt(prompt("Ingrese la base del cuadrado"));
+    var altura = parseInt(prompt("Ingrese la altura del cuadrado"));
+
+    alert("El área del cuadrado es " + (base * altura));
+}
+
+function perimetro_circulo() {
+
+    var radio = parseInt(prompt("Ingrese el radio del circulo"))
+    alert("El perimetro del circulo es " + (2 * Math.PI * radio))
+}
+
+
+function numeroParImpar() {
+
+    var n1 = parseInt(prompt("Ingrese un numero: "));
+    if (n1 % 2 == 0) {
+        alert('Es par')
+    } else {
+        alert('Es impar')
+    }
+
+}
+
+function edadPersona() {
+
+    var edad = parseInt(prompt("Ingrese su edad: "));
+    if (edad <= 10 && edad > 10) {
+        alert("Usted es un niñe")
+    } else if (edad > 10 && edad < 18) {
+        alert("Usted es un adolecente")
+    } else if (edad <= 0 || edad > 150) {
+        alert("Usted no existe :(")
+    } else {
+        alert("Usted esta grandotQ")
+    }
+}
+
+function ejercicioMatematico1() {
+
+    var n1 = parseInt(prompt("Ingrese primer numero "));
+    var n2 = parseInt(prompt("Ingrese segundo numero "));
+    var cuadrado1 = n1 * n1;
+    var cuadrado2 = n2 * n2;
+    if (cuadrado1 >= n2) {
+        alert("Modulo " + (cuadrado1 % n2))
+    } else {
+        alert("Resta " + (cuadrado2 - n1))
+    }
+}
+
+function mayorDeTresNum() {
+
+    var v1, v2, v3
+    v1 = parseInt(prompt("Ingrese primer numero "));
+    v2 = parseInt(prompt("Ingrese segundo numero "));
+    v3 = parseInt(prompt("Ingrese tercer numero "));
+
+    if (v1 > v2 && v1 > v3) {
+        alert("el mayor de los numeros es " + v1)
+    } else if (v2 > v1 && v2 > v3) {
+        alert("el mayor de los numeros es " + v2)
+    } else {
+        alert("el mayor de los numeros es " + v3)
+    }
+}
