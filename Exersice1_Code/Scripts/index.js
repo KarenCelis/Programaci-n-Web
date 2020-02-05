f018e1b2b2ace2058cc5670053afe32b379e2738
Karen Celis <k-celis@javeriana.edu.co>
2020-02-05 14:56:52 -0500

    Excercise to Integration hmtl.css.javaScript

---
+++ Exersice1_Code/Scripts/index.js
@@ -0,0 +1,12 @@
+//sobreescribe el documento, si existe algo lo elimina y escribe lo que se le pide
+//sobreescribe lo que ya existe
+//document.write("ANTU CHIMUELO")
+
+function main() {
+    //  alert("hello");
+    //  console.log("Hi little panther")
+    //No usarlo en funciones 
+    // document.write("AMARU")
+    //.innerhtml cambiar el contenido del elemento y el contenido puede ser texto o html
+    document.getElementById("container").innerHTML = "<h1>Black Cats</h1>"
+}
