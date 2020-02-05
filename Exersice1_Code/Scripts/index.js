583952d0e6173e6f690868fb5abed58cb284b397
Karen Celis <k-celis@javeriana.edu.co>
2020-02-05 15:15:47 -0500

    concat in js to html

--- Exersice1_Code/Scripts/index.js
+++ Exersice1_Code/Scripts/index.js
@@ -9,4 +9,8 @@ function main() {
     // document.write("AMARU")
     //.innerhtml cambiar el contenido del elemento y el contenido puede ser texto o html
     document.getElementById("container").innerHTML = "<h1>Black Cats</h1>"
+        //en vez de sobreescribir se puede concatenar
+    document.getElementById("container").innerHTML += "<h2>ANTU</h2>"
+
+    document.getElementById("container").innerHTML += "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>"
 }
