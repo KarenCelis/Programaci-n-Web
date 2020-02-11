a5e84f3f51a24f9198507a5d7e2d920ca61de796
Karen Celis <k-celis@javeriana.edu.co>
2020-02-11 16:47:05 -0500

    first functions with buttons

--- Calculator/scripts/index.js
+++ Calculator/scripts/index.js
@@ -5,3 +5,22 @@ function writeC(message) {
     content += message
     result1.innerHTML = content
 }
+
+function deleteEverything() {
+    content = ""
+    result1.innerHTML = content
+}
+
+function deleteOne() {
+
+    if (content != "") {
+        var last = content.charAt(content.length - 1)
+        if (last == " ") {
+            content = content.substr(0, content.length - 3)
+        } else {
+            content = content.substr(0, content.length - 1)
+        }
+        result1.innerHTML = content
+
+    }
+}
