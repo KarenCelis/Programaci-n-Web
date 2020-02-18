878d0f46477c622f6b6f150b6db1bd3279b483a2
Karen Celis <k-celis@javeriana.edu.co>
2020-02-18 14:35:12 -0500

    Excercise Array Object

---
+++ Excercise_Array_object/scripts/index.js
@@ -0,0 +1,4 @@
+//array.sort()->muestra un arreglo de menor a mayor
+//array.reverse()->muestra el arreglo de mayor a menor
+//array.join()-> une todos los elementos del arregloen una sola cadena de texto, se puede colocar un separador ("-")
+//array.split()-> divide la cadena de texto a un array
