383462dbdbdc4112689fc69cda1406038c1b441e
Karen Celis <k-celis@javeriana.edu.co>
2020-02-18 16:19:11 -0500

    Update index.js

--- Excercise_Array_object/scripts/index.js
+++ Excercise_Array_object/scripts/index.js
@@ -28,3 +28,139 @@ while (todosCarros.length > 0) {
     console.log(todosCarros.pop())
 }
 console.log(todosCarros)
+    //(----------------------------OBJECTS------------------------------)
+    //el objeto define algo
+var persona = {
+        'Nombre': "Amaru",
+        'Apellido': "Antu",
+        'Edad': "1",
+        'Telefono': "234567",
+        'FechaNacimiento': new Date(2018, 11, 1),
+        'casado': false,
+        //Agregar una funcion al objeto
+        'Hablar': sohwMessage,
+        'presentarse': function() {
+            //this atributo propio del objeto dentro del mismo objeto
+            alert("Hi my name is " + this.Nombre)
+        },
+        'saludar': function() {
+
+            alert("Hi other person " + otraPersona.nombre)
+        }
+    }
+    //JSON JS Objects Notation
+var otraPersona = {
+
+    'nombre': "Michin",
+    'Apellido': "Amarillo",
+    'Edad': "3",
+    'Telefono': "23456745"
+
+}
+
+function sohwMessage() {
+
+    alert("Hello World")
+}
+console.log(persona)
+console.log(persona.Apellido)
+console.log(persona.FechaNacimiento.getMonth())
+
+// -------------------------------------------------------------
+var starWars = {
+    'Nombre': "Star Wars Episodio I",
+    'FechaEstreno': new Date(1977),
+    'DuracionMinutos': 120,
+    'Genero': "Ciencia Ficcion",
+    'Clasificacion': "A",
+    'Nominaciones': 12,
+    'Personajes': [
+        "Dath Vader",
+        "Luke Skywalker",
+        "Leia Organa",
+        "Han Solo",
+        "Chewbaca"
+    ]
+}
+
+var menInBlack = {
+    'Nombre': "Men in BLack",
+    'FechaEstreno': new Date(1997),
+    'DuracionMinutos': 98,
+    'Genero': "Accion",
+    'Clasificacion': "PG-13",
+    'Nominaciones': 3,
+    'Personajes': [
+        "Agente K",
+        "Agente J",
+        "Angete L",
+        "Frak el perro"
+
+    ]
+}
+var candelaria = {
+    'Nombre': "Candelaria",
+    'FechaEstreno': new Date(2017),
+    'DuracionMinutos': 88,
+    'Genero': "Cine Dramatico",
+    'Clasificacion': "PG-13",
+    'Nominaciones': 10,
+    'Personajes': [
+        "Victor Hugo",
+        "Candelaria"
+
+    ]
+}
+var sherk = {
+    'Nombre': "Sherk",
+    'FechaEstreno': new Date(2001),
+    'DuracionMinutos': 92,
+    'Genero': "Animació ",
+    'Clasificacion': "PG",
+    'Nominaciones': 40,
+    'Personajes': [
+        "Sherk",
+        "Princesa Fiona",
+        "Burro",
+        "Lord Farquaad",
+        "Jengibre"
+
+    ]
+}
+var harryPotter = {
+    'Nombre': "Harry Potter y la Piedra Filosofal",
+    'FechaEstreno': new Date(2001),
+    'DuracionMinutos': 152,
+    'Genero': "Fantasia",
+    'Clasificacion': "PG",
+    'Nominaciones': 30,
+    'Personajes': [
+        "Harry Potter",
+        "Ron Weaslye",
+        "Hermione Granger",
+        "Albus Dumbledore",
+        "Rubeus Hagrid",
+        "Severus Snape",
+        "Minerva McGonagall"
+
+    ]
+}
+
+var comoEntrenarATuDragon = {
+    'Nombre': "Cómo entrenar a tu dragón",
+    'FechaEstreno': new Date(2010),
+    'DuracionMinutos': 98,
+    'Genero': "Animación",
+    'Clasificacion': "PG",
+    'Nominaciones': 20,
+    'Personajes': [
+        "Stoick",
+        "Hipo",
+        "Gobber",
+        "Astrid",
+        "Chimuelo",
+        "Brutilda Torton",
+        "Brutacio Torton"
+
+    ]
+}
