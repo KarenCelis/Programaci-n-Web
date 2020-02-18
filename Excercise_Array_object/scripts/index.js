4c93284cadda826f133bbb816098dd91d935f39c
Karen Celis <k-celis@javeriana.edu.co>
2020-02-18 14:52:21 -0500

    Update index.js

--- Excercise_Array_object/scripts/index.js
+++ Excercise_Array_object/scripts/index.js
@@ -2,3 +2,23 @@
 //array.reverse()->muestra el arreglo de mayor a menor
 //array.join()-> une todos los elementos del arregloen una sola cadena de texto, se puede colocar un separador ("-")
 //array.split()-> divide la cadena de texto a un array
+//array.push("qw","sdf","rrrr")-> agregar elementos al arreglo
+//array.pop()->devuelve solo el ultimo elemnto del arreglo y lo saco
+//array.concat()->concatena a este arreglo elementos de otro arreglo
+/*
+var array = []
+array.sort()
+array.reverse()
+array.join()
+array.push("New Element")
+var elements = []
+elements.push(array.pop())
+array.concat(["Other Elemnts"])*/
+
+var carrosCaros = ["Alfa", "Porshe", "Masserati"]
+var carrosBaratos = ["Renault", "Chevrolet", "Mazda"]
+var todosCarros = carrosCaros.concat(carrosBaratos)
+todosCarros = todosCarros.sort()
+var lista = todosCarros.join("\n")
+console.log(lista)
+console.log(todosCarros.length)
