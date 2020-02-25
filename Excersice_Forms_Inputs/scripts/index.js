62598943670363598bb15bf6ede18b2a6c86c5d1
Karen Celis <k-celis@javeriana.edu.co>
2020-02-24 20:42:48 -0500

    Change name

---
+++ Excersice_Forms_Inputs/scripts/index.js
@@ -0,0 +1,12 @@
+function cambiarFondo() {
+
+    var colorInput = document.getElementById("colorFondo").value
+    document.body.style.backgroundColor = colorInput
+}
+
+function cambiarTamanoLetra() {
+
+
+    var tam = document.getElementById("tamanoL").value
+    document.body.style.fontSize = tam
+}
